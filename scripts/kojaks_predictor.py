810ba670f4905ff8681b3ca6eec6971bf42e4d40
Rachel Yang <ry2277@columbia.edu>
2017-05-23 21:52:07 -0400

    kojaks_predictor calls classify_main in classify_image.py

--- scripts/kojaks_predictor.py
+++ scripts/kojaks_predictor.py
@@ -1,3 +1,5 @@
+import classify_image
+
 # kojaks_predictor.py
 # Input: cv image, true obs_car translation relative to velodyne link on capture car
 # Output: Predicted obs_car translation relative to the velodyne link on the capture car
@@ -5,4 +7,6 @@
 # cv_image is an image accepted by OpenCV 2
 # pose is a len-3 [tx, ty, tz] array representing the obs_car's translation relative to the velodyne link on the capture car
 def run_predictor_on_frame(cv_image, laser_points, true_pose):
+	classify_image.classify_main(cv_image)
 	return [10.0, 10.0, -1.0] 
+

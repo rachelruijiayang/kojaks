68bd5bc69d89123052051e8f5a9897555a542cf7
Rachel Yang <ry2277@columbia.edu>
2017-05-30 16:10:15 -0400

    indents

--- scripts/kojaks_predictor.py
+++ scripts/kojaks_predictor.py
@@ -39,14 +39,14 @@ class KojaksPredictor:
 		return gen_pose
 
 	def run_laser_predictor(self, laser_arr):
-"""
+		"""
 		p = pcl.PointCloud(np.array([[1, 2, 3], [3, 4, 5]], dtype=np.float32))
 		seg = p.make_segmenter()
 		seg.set_model_type(pcl.SACMODEL_PLANE)
 		seg.set_method_type(pcl.SAC_RANSAC)
 		#indices, model = seg.segment()
 		#print model
-"""
+		"""
 
 	def writeTrainingPairsToFile(self, filename):
 		with open(filename, 'w') as outfile:

4d79252044a4cd567813a5ca31a96d922a663a0b
Rachel Yang <ry2277@columbia.edu>
2017-05-30 15:48:40 -0400

    if no bbox

--- scripts/kojaks_predictor.py
+++ scripts/kojaks_predictor.py
@@ -29,9 +29,9 @@ class KojaksPredictor:
 		# image handling
 		print("true pose of the car is: " + str(true_pose))
 		yolo_result = self.yolo.detect_from_cvmat(cv_image)
-		if yolo_result != None:
-			self.training_pairs.append([yolo_result[0], true_pose])
 		print("yolo 2d bboxes are " + str(yolo_result)) # yolo_result is in the format [['car', 756.87244, 715.84973, 343.4021, 304.45911, 0.80601584911346436]]
+		if yolo_result != []:
+			self.training_pairs.append([yolo_result[0], true_pose])
 		gen_pose = self.transform2DBboxTo3DPoint(yolo_result) # gen_pose is in the format [x, y, z]
 		print("generated pose of the car is: " + str(gen_pose) + "\n")
 
